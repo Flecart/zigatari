98c9dba265ab6fa0bcca8e7082b90865de4a6759
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-09-19 00:03:36 +0200

    finish 2.2, add defers

--- src/trygl.zig
+++ src/trygl.zig
@@ -58,11 +58,12 @@ pub fn main() !void {
 
     const proc: glfw.GLProc = undefined;
     try gl.loadExtensions(proc, glGetProcAddress);
+
     // build and compile our shader program
     // ------------------------------------
     // vertex shader
-
     const vertexShader = gl.createShader(gl.ShaderType.vertex);
+    defer gl.deleteShader(vertexShader);
     gl.shaderSource(vertexShader, 1, &vertexShaderSource);
     gl.compileShader(vertexShader);
 
@@ -80,6 +81,7 @@ pub fn main() !void {
 
     // fragment shader
     const fragmentShader = gl.createShader(gl.ShaderType.fragment);
+    defer gl.deleteShader(fragmentShader);
     gl.shaderSource(fragmentShader, 1, &fragmentShaderSource);
     gl.compileShader(fragmentShader);
 
@@ -92,6 +94,7 @@ pub fn main() !void {
 
     // link shaders
     const shaderProgram = gl.createProgram();
+    defer gl.deleteProgram(shaderProgram);
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
@@ -103,24 +106,23 @@ pub fn main() !void {
         });
     }
 
-    gl.deleteShader(vertexShader);
-    gl.deleteShader(fragmentShader); // why?
-
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
 
     const vertices = [_]f32{
+        // Original triangle coordinates
+        // -0.5, -0.5, 0.0,
+        // 0.5, -0.5, 0.0,
+        // 0.0,  0.5, 0.0
 
-    -0.5, -0.5, 0.0,
-     0.5, -0.5, 0.0,
-     0.0,  0.5, 0.0
-        // 0.5, 0.5, 0.0, // top-right
-        // 0.5, -0.5, 0.0, // bottom-right
-        // -0.5, -0.5, 0.0, // bottom-left
-        // -0.5, 0.5, 0.0, // top-right
+        // Down here the coordinates used in the tutorial C code
+        0.5,  0.5, 0.0,  // top right
+        0.5, -0.5, 0.0,  // bottom right
+        -0.5, -0.5, 0.0,  // bottom left
+        -0.5,  0.5, 0.0   // top left 
     };
 
-    const indices = [_]f32{
+    const indices = [_]u32{
         0, 1, 3, // first triangle
         1, 2, 3, // second triangle
     };
@@ -135,7 +137,6 @@ pub fn main() !void {
         gl.deleteVertexArray(vao);
         gl.deleteBuffer(vbo);
         gl.deleteBuffer(ebo);
-        gl.deleteProgram(shaderProgram);
     }
 
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
@@ -145,23 +146,23 @@ pub fn main() !void {
     gl.bufferData(gl.BufferTarget.array_buffer, f32, &vertices, gl.BufferUsage.static_draw);
 
     gl.bindBuffer(ebo, gl.BufferTarget.element_array_buffer);
-    gl.bufferData(gl.BufferTarget.element_array_buffer, f32, &indices, gl.BufferUsage.static_draw);
+    gl.bufferData(gl.BufferTarget.element_array_buffer, u32, &indices, gl.BufferUsage.static_draw);
 
     gl.vertexAttribPointer(0, 3, gl.Type.float, false, 3 * @sizeOf(f32), 0);
     gl.enableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered vbo as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    gl.bindBuffer(gl.Buffer.invalid,  gl.BufferTarget.array_buffer);
+    // gl.bindBuffer(gl.Buffer.invalid,  gl.BufferTarget.array_buffer);
 
     // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-    // gl.bindBuffer(gl.binding.ELEMENT_ARRAY_BUFFER, 0);
+    // gl.bindBuffer(gl.Buffer.invalid, gl.BufferTarget.element_array_buffer);
 
     // You can unbind the vao afterwards so other vao calls won't accidentally modify this vao, but this rarely happens. Modifying other
     // vaos requires a call to glBindVertexArray anyways so we generally don't unbind vaos (nor vbos) when it's not directly necessary.
-    gl.bindVertexArray(gl.VertexArray.invalid);
+    // gl.bindVertexArray(gl.VertexArray.invalid);
 
     // uncomment this call to draw in wireframe polygons.
-    // gl.polygonMode(gl.binding.FRONT_AND_BACK, gl.binding.LINE);
+    gl.polygonMode(gl.CullMode.front_and_back, gl.DrawMode.line);
 
     // render loop
     // -----------
@@ -176,23 +177,14 @@ pub fn main() !void {
         // draw our first triangle
         gl.useProgram(shaderProgram);
         gl.bindVertexArray(vao); // seeing as we only have a single vao there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        gl.drawArrays(gl.PrimitiveType.triangles, 0, 3);
-        // gl.drawElements(gl.PrimitiveType.triangles, 6, gl.ElementType.u32, 0);
-        // glBindVertexArray(0); // no need to unbind it every time
+        // gl.drawArrays(gl.PrimitiveType.triangles, 0, 3);
+        gl.drawElements(gl.PrimitiveType.triangles, 6, gl.ElementType.u32, 0);
+        // gl.bindVertexArray(gl.VertexArray.invalid); // no need to unbind it every time
 
 
         window.swapBuffers();
         glfw.pollEvents();
     }
-
-
-    // Wait for the user to close the window.
-    // while (!window.shouldClose()) {
-
-    //     gl.clearColor(1, 0, 1, 1);
-    //     gl.clear(.{.color = true});
-
-    // }
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
