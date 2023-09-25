e0da1f84e5ef4f8ebbf24da396071dd5c6b07754
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-09-25 21:55:57 +0200

    create game object

---
+++ src/game_object.zig
@@ -0,0 +1,42 @@
+const zlm = @import("zlm");
+const Texture = @import("texture.zig");
+const Renderer = @import("sprite_renderer.zig");
+
+const Self = @This();
+
+postition: zlm.Vec2,
+size: zlm.Vec2,
+velocity: f32,
+color: zlm.Vec3,
+rotation: f32,
+isSolid: bool,
+destroyed: bool,
+sprite: Texture,
+
+pub const default = Self {
+    .position = zlm.vec2{0.0, 0.0},
+    .size = zlm.vec2{10.0, 10.0},
+    .velocity = 1.0,
+    .color = zlm.vec3{1.0, 1.0, 1.0},
+    .rotation = 0.0,
+    .isSolid = false,
+    .destroyed = false,
+    .sprite = undefined,
+};
+
+pub fn init(position: zlm.Vec2, size: zlm.Vec2, sprite: Texture, color: zlm.Vec3, velocity: zlm.Vec2) Self {
+    return Self {
+        .position = position,
+        .size = size,
+        .velocity = velocity,
+        .color = color,
+        .rotation = 0.0,
+        .isSolid = false,
+        .destroyed = false,
+        .sprite = sprite,
+    };
+}
+
+pub fn draw(self: Self, renderer: Renderer) void {
+    renderer.drawSprite(self.sprite, self.sprite, self.position, self.size, self.rotation, self.color);
+}
